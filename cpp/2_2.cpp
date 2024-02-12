2a328a464f2ac3463024a794199c273317a50ec7
barsikkeglya <jorikinfo99@gmail.com>
2024-02-12 13:48:48 +0300

    shit task

--- cpp/2_2.cpp
+++ cpp/2_2.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 using namespace std;
-
-bool is_powerof2(uint64_t a) {
-    if (a == 1)
-        return true;
-    while ((a != 0) and (a != 1)){
-        if (a % 2 != 0)
-            return false;
-        a = a / 2;
-    }
-    return true;
+#include <math.h>
+bool is_square(uint64_t num) {
+    double r = sqrt(num);
+    return ((std::ceil(r)*std::ceil(r) == num) or (std::floor(r)*std::floor(r) == num));
 }
-
 int main(){
-    uint64_t a = 2;
-    cout << is_powerof2(a);
+    uint64_t a = 18446744065119617026;
+    cout << a;
     return 0;
 }
