b40d9aab98a447e8068c3a7d3ad3bf036a3a7686
barsikkeglya <jorikinfo99@gmail.com>
2024-02-25 22:26:48 +0300

    bugfixes

--- 2_semester/cpp_labs/lab_1/search/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/George/Documents/mipt_dgap
-# Build directory: C:/Users/George/Documents/mipt_dgap/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
