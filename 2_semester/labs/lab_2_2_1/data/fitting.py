f066017bb404eae3649a5e7b63267d6e02fa5cc7
barsikkeglya <jorikinfo99@gmail.com>
2024-03-30 15:46:13 +0300

    test commit please ignore

--- 2_semester/labs/lab_2_2_1/data/fitting.py
+++ 2_semester/labs/lab_2_2_1/data/fitting.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-df = pd.read_csv('C:/Users/George/Documents/mipt_dgap/2_semester/labs/lab_2_2_1/data/20240326_1711439001210_41.csv', sep=',', header=0)
+df = pd.read_csv('C:/mipt_dgap/2_semester/labs/lab_2_2_1/data/20240326_1711439001210_41.csv', sep=',', header=0)
 
 data = df.to_numpy()
 #print(data)
@@ -21,4 +22,13 @@ sigmanp = np.array(sigmat)
 f = lambda x, u0, tau: u0*pow(2.71828,-x/tau)
 
 popt, pcov = curve_fit(f,tnp,unp,sigma = sigmanp)
-print(popt)
+uf = popt[0]
+tf = popt[1]
+
+xlin = np.linspace(0,600,1000)
+plt.figure(figsize=(6,6))
+plt.plot(xlin,f(xlin,uf,tf))
+plt.title('dasgsdfddsa funny plot')
+plt.xlabel('funny xlabel')
+plt.savefig('C:/mipt_dgap/2_semester/labs/lab_2_2_1/data/fig1.eps',format='eps')
+plt.show()
